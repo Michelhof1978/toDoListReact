fb72f9ed8d925bfcd960feebcbccdefa78667033
hoffmann michel <michel.hof@hotmail.fr>
2023-05-17 13:52:52 +0100

    uuid

---
+++ APPLICATION/.history/src/App_20230517130914.js
@@ -0,0 +1,15 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Form from './Components/Form'
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1 className="text-center mt-3">Todo-List</h1>
+        <Form />         {/* On affiche le composant form */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ APPLICATION/.history/src/Components/Form_20230517124642.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+//COMPONENTS
+import Item from './Item'
+
+export default function Form()  {
+    return (
+
+        <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
+
+            <form className="mb-3">
+                <label htmlFor="todo" className="form-label mt-3">Chose à faire</label>
+                <input type="text" className="form-control" id="todo"/>
+                {/* input est relié à label */}
+
+                <button className="mt-2 btn btn-primary d-block">Envoyez</button>
+            </form>
+
+            <h2>Liste des choses à faire : </h2>
+            <ul className="list-group">
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+            </ul>   
+        </div>
+
+    )
+}
